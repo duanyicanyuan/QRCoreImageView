b78b76c8e2f8d96ce0547e40402b63ea047cf684
caiqiujun <caiqiujun@caiqiujundemacbook-pro.local>
2016-01-15 13:55:04 +0800

    Initial Commit

---
+++ QRCoreStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCoreStudy
+//
+//  Created by caiqiujun on 16/1/15.
+//  Copyright © 2016年 caiqiujun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
